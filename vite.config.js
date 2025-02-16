0a0a4fa7871a16613349dc4f0b8ad662e2b2d225
H0reka <horeka.app@gmail.com>
2025-02-16 21:46:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://15.206.209.231:8080/admin',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/noadminapi': {
-        target: 'http://15.206.209.231:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/noadminapi/, ''),
-      },
-    },
-  },
 })
